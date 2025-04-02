251f21f593a6789c262ee44dc5fe6c0963dab3bd
ZAHON <konrad.zahon@gmail.com>
2025-04-02 09:58:52 +0200

    feat(core): add validation that check if `controlledSignal` prop in `useUncontrolled` hook is object of type `Signal`

--- core/src/hooks/use-uncontrolled/use-uncontrolled.ts
+++ core/src/hooks/use-uncontrolled/use-uncontrolled.ts
@@ -1,12 +1,17 @@
 import type { ReadonlySignal } from '@builder.io/qwik';
 import type { UseUncontrolledParams } from './use-uncontrolled.types';
 import { useSignal, $ } from '@builder.io/qwik';
+import { isDev, isSignal } from '@builder.io/qwik';
 
 /**
  * Manage state of both controlled and uncontrolled components.
  */
 export const useUncontrolled = <T>(params: UseUncontrolledParams<T>) => {
   const { controlledSignal, uncontrolledValue, finalValue, onChange$ } = params;
+
+  if (isDev && !isSignal(controlledSignal)) {
+    throw new Error('Rilix UI: useUncontrolled hook controlledSignal prop must be object of type Signal.');
+  }
 
   // If a controlled signal has been passed, we return it immediately.
   if (controlledSignal !== undefined) {
