9e41415ac81780cd3231f6d04cb556b5f208edfd
ZAHON <konrad.zahon@gmail.com>
2025-05-24 00:30:27 +0200

    docs(core): enhance clarity of `as` prop description within `VisuallyHiddenRootProps` interface

--- core/src/components/visually-hidden/parts/visually-hidden-root/visually-hidden-root.types.ts
+++ core/src/components/visually-hidden/parts/visually-hidden-root/visually-hidden-root.types.ts
@@ -2,9 +2,7 @@ import type { PropsOf, FunctionComponent } from '@builder.io/qwik';
 
 export interface VisuallyHiddenRootProps extends PropsOf<'span'> {
   /**
-   * Change the default rendered element for the one passed as, merging their props and behavior.
-   *
-   * Read our [Composition](https://github.com/ZAHON/rilix-ui/blob/main/core/docs/guides/composition.md) guide for more details.
+   * The component that this component should render as.
    */
   as?: FunctionComponent;
 }
