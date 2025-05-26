c818808a8da083ffde835880c4612f3b020420cd
ZAHON <konrad.zahon@gmail.com>
2025-05-27 01:31:16 +0200

    feat(core): optimize `composeRefs` utility function for `Qwik` serialization and resumability

--- core/src/utilities/compose-refs/compose-refs.ts
+++ core/src/utilities/compose-refs/compose-refs.ts
@@ -1,18 +1,20 @@
 import type { PossibleRef } from './compose-refs.types';
 import type { Signal } from '@builder.io/qwik';
-import { isSignal } from '@builder.io/qwik';
+import { $, isSignal, noSerialize } from '@builder.io/qwik';
 
 /**
  * A utility function that allows assigning a single DOM element to multiple refs.
  */
 export const composeRefs = <T extends Element = Element>(refs: PossibleRef<T>[]) => {
-  return (node: T) => {
-    refs?.forEach((ref) => {
+  const _refs = noSerialize(refs);
+
+  return $((node: T) => {
+    _refs?.forEach((ref) => {
       if (typeof ref === 'function') {
         ref(node);
       } else if (ref !== undefined && isSignal(ref)) {
         (ref as Signal<T>).value = node;
       }
     });
-  };
+  });
 };
