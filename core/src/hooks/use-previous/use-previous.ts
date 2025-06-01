f6489ec79d1bfd7f9d3172d5842b039d5beb2322
ZAHON <konrad.zahon@gmail.com>
2025-06-01 23:58:07 +0200

    refactor(core): remove development-only type check from `usePrevious` hook
    Previously, the `usePrevious` hook included a development-only check to ensure that the `value` parameter was a Qwik `Signal`. This check would throw an error in development mode if a non-Signal value was provided.

--- core/src/hooks/use-previous/use-previous.ts
+++ core/src/hooks/use-previous/use-previous.ts
@@ -1,7 +1,6 @@
 import type { UsePreviousParams } from './use-previous.types';
 import type { ReadonlySignal } from '@builder.io/qwik';
 import { useSignal, useTask$ } from '@builder.io/qwik';
-import { isDev, isSignal } from '@builder.io/qwik';
 
 /**
  * Tracks and returns the previous value of a given signal.
@@ -11,10 +10,6 @@ export const usePrevious = <T>(params: UsePreviousParams<T>) => {
 
   const currentValue = useSignal<T>(value.value);
   const previousValue = useSignal<T | undefined>(undefined);
-
-  if (isDev && !isSignal(value)) {
-    throw new Error(`Rilix UI: The 'value' param in 'usePrevious' hook must be a Signal. Received: ${typeof value}.`);
-  }
 
   useTask$(({ track }) => {
     track(() => value.value);
