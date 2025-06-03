f33d2a7508951f995fdb0ec8e5fd9825ad4112f1
ZAHON <konrad.zahon@gmail.com>
2025-06-03 09:18:51 +0200

    chore(core): adjust ESLint rule for empty object types
    Updated the ESLint configuration by setting `@typescript-eslint/no-empty-object-type` to 'off'. This change provides more flexibility when defining empty object types in TypeScript, aligning with current project needs.

--- core/.eslintrc.cjs
+++ core/.eslintrc.cjs
@@ -20,6 +20,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
+    '@typescript-eslint/no-empty-object-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
