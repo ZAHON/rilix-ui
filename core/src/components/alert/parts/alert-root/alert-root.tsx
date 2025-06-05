d3eebc99126a998bc9443067cfe75e4af5054262
ZAHON <konrad.zahon@gmail.com>
2025-06-05 02:45:35 +0200

    feat(core)!: enhance `Alert.Root` with `Render` component, introducing `render$` prop and removing `as` prop
    This commit refactors the `Alert.Root` component to leverage the newly introduced `Render` component internally. This significantly enhances its flexibility and composability for Rilix UI.
    
    The previous `as?: Component` prop has been removed, and a powerful new `render$` prop has been introduced. This allows consumers to:
    
    * Fully control the rendering of `Alert.Root`'s underlying element, enabling a more dynamic and custom UI.
    * Compose `Alert.Root` with other components, inheriting its intended behavior and accessibility while offering complete rendering freedom.
    
    This change streamlines the component's internal structure and aligns `Alert.Root` with the broader Rilix UI strategy for flexible component composition.

--- core/src/components/alert/parts/alert-root/alert-root.tsx
+++ core/src/components/alert/parts/alert-root/alert-root.tsx
@@ -1,19 +1,25 @@
 import type { AlertRootProps } from './alert-root.types';
 import { component$, Slot } from '@builder.io/qwik';
-import { Primitive } from '@/components';
+import { Render } from '@/_internal';
 
 /**
  * Contains the content for the alert.
  * This component is based on the `div` element.
  */
 export const AlertRoot = component$<AlertRootProps>((props) => {
-  const { as, ...others } = props;
-
-  const Component = as || (Primitive.div as unknown as 'div');
-
   return (
-    <Component role="alert" data-rilix-ui-alert-root="" data-scope="alert" data-part="root" {...others}>
-      <Slot />
-    </Component>
+    <Render
+      as="div"
+      role="alert"
+      data-rilix-ui-alert-root=""
+      data-scope="alert"
+      data-part="root"
+      defaultRender$={(props) => (
+        <div {...props}>
+          <Slot />
+        </div>
+      )}
+      {...props}
+    />
   );
 });
