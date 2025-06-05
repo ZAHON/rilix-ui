ccc1a54e7119daedcfe38ad578a9b027c6b0ada2
ZAHON <konrad.zahon@gmail.com>
2025-06-06 00:10:03 +0200

    feat(core)!: enhance `AspectRatio.Content` component composition with `render$` prop
    Replaced the `as` prop with a more flexible `render$` prop in `AspectRatio.Content` for advanced content customization.

--- core/src/components/aspect-ratio/parts/aspect-ratio-content/aspect-ratio-content.tsx
+++ core/src/components/aspect-ratio/parts/aspect-ratio-content/aspect-ratio-content.tsx
@@ -1,6 +1,6 @@
 import { AspectRatioContentProps } from './aspect-ratio-content.types';
 import { component$, useContext, useComputed$, Slot } from '@builder.io/qwik';
-import { Primitive } from '@/components';
+import { Render } from '@/_internal';
 import { combineStyle } from '@//utilities';
 import { AspectRatioContext } from '../../context';
 
@@ -9,7 +9,7 @@ import { AspectRatioContext } from '../../context';
  * This component is based on the `div` element.
  */
 export const AspectRatioContent = component$<AspectRatioContentProps>((props) => {
-  const { as, style, ...others } = props;
+  const { style, ...others } = props;
 
   const { aspect } = useContext(AspectRatioContext);
 
@@ -17,18 +17,21 @@ export const AspectRatioContent = component$<AspectRatioContentProps>((props) =>
     combineStyle({ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }, style)
   );
 
-  const Component = as || (Primitive.div as unknown as 'div');
-
   return (
-    <Component
+    <Render
+      as="div"
       data-rilix-ui-aspect-ratio-content=""
       data-scope="aspect-ratio"
       data-part="content"
       data-aspect={aspect.value}
       style={combinedStyle.value}
+      state={{ aspect }}
+      defaultRender$={(props) => (
+        <div {...props}>
+          <Slot />
+        </div>
+      )}
       {...others}
-    >
-      <Slot />
-    </Component>
+    />
   );
 });
--- core/src/components/aspect-ratio/parts/aspect-ratio-content/aspect-ratio-content.types.ts
+++ core/src/components/aspect-ratio/parts/aspect-ratio-content/aspect-ratio-content.types.ts
@@ -1,8 +1,25 @@
-import type { PropsOf, Component } from '@builder.io/qwik';
+import type { PropsOf, ReadonlySignal, JSXOutput } from '@builder.io/qwik';
 
 export interface AspectRatioContentProps extends PropsOf<'div'> {
   /**
-   * The component that this component should render as.
+   * Allows you to replace the component’s HTML element with a different tag, or compose it with another component.
+   * Read our [Composition](https://github.com/ZAHON/rilix-ui/blob/main/core/docs/guides/composition.md) guide for more details.
    */
-  as?: Component;
+  render$?: (
+    /**
+     * These are the standard HTML attributes and properties that should be applied to your custom rendered element.
+     * Spreading these props ensures that your component maintains its intended behavior, accessibility features, and proper integration with the DOM.
+     */
+    props: Record<string, unknown>,
+
+    /**
+     * An object that provides access to the internal state of the component.
+     */
+    state: {
+      /**
+       * The current aspect ratio (in %).
+       */
+      aspect: ReadonlySignal<number>;
+    }
+  ) => JSXOutput;
 }
