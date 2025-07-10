486b5a325b9873f2368e6ef51affe372274a5227
ZAHON <konrad.zahon@gmail.com>
2025-07-10 21:51:03 +0200

    feat(core)!: remove `setStyleProperty` utility function

--- core/src/utilities/index.ts
+++ core/src/utilities/index.ts
@@ -1,6 +1,5 @@
 export * from './clamp';
 export * from './combine-style';
 export * from './compose-refs';
-export * from './set-style-property';
 export * from './string-style-to-object';
 export * from './visually-hidden-style';
--- core/src/utilities/set-style-property/index.ts
+++
@@ -1,2 +0,0 @@
-export type { SetStylePropertyParams } from './set-style-property.types';
-export { setStyleProperty } from './set-style-property';
