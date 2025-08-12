b628bfb391c9bfa8e186ed806f864f1c572c2214
ZAHON <konrad.zahon@gmail.com>
2025-08-12 10:03:36 +0200

    refactor(core): refine `data-rilix-ui-alert-root` attribute in `Alert.Root` component
    Changes the `data-rilix-ui-alert-root` attribute to a boolean attribute without a value in the `Alert.Root` component.
    This refines the component's markup, ensuring it's semantically correct and consistent with HTML standards for boolean data attributes.

--- core/src/components/alert/parts/alert-root/alert-root.tsx
+++ core/src/components/alert/parts/alert-root/alert-root.tsx
@@ -11,7 +11,7 @@ export const AlertRoot = component$<AlertRootProps>((props) => {
     <Render
       as="div"
       role="alert"
-      data-rilix-ui-alert-root=""
+      data-rilix-ui-alert-root
       defaultRender$={(props) => (
         <div {...props}>
           <Slot />
