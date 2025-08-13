ccc642b1823685edb2d69dae3e988b0cfded8a97
ZAHON <konrad.zahon@gmail.com>
2025-08-13 22:57:25 +0200

    docs(core): refine JSDoc comments for `SeparatorContextValue`
    Updated the JSDoc comments for the `orientation` and `decorative` properties in the `SeparatorContextValue` interface.
    The new descriptions are more consistent and clearly state that the signals return the component's state values.

--- core/src/components/separator/contexts/separator-context/separator-context.types.ts
+++ core/src/components/separator/contexts/separator-context/separator-context.types.ts
@@ -2,13 +2,13 @@ import type { ReadonlySignal } from '@builder.io/qwik';
 
 export interface SeparatorContextValue {
   /**
-   * The orientation of the component.
+   * A readonly signal whose value is the orientation of the component.
    */
   orientation: ReadonlySignal<'horizontal' | 'vertical'>;
 
   /**
-   * Whether or not the component is purely decorative.
-   * When `true`, accessibility-related attributes are updated so that that the rendered element is removed from the accessibility tree.
+   * A readonly signal whose value indicates whether or not the component is purely decorative.
+   * When `true`, accessibility-related attributes are updated so that the rendered element is removed from the accessibility tree.
    */
   decorative: ReadonlySignal<boolean>;
 }
