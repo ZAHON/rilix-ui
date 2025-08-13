7ae70d5f2f8acb577faab0c18ac927d0139de017
ZAHON <konrad.zahon@gmail.com>
2025-08-13 22:45:19 +0200

    refactor(core): update `SeparatorContextValue` to use `boolean` for `decorative` prop
    Updated the `SeparatorContextValue` interface to change the type of the `decorative` `ReadonlySignal` from `boolean | undefined` to `boolean`.
    This change reflects the component's internal logic, where the `decorative` prop now always resolves to a boolean value, improving type safety and consistency.

--- core/src/components/separator/contexts/separator-context/separator-context.types.ts
+++ core/src/components/separator/contexts/separator-context/separator-context.types.ts
@@ -10,5 +10,5 @@ export interface SeparatorContextValue {
    * Whether or not the component is purely decorative.
    * When `true`, accessibility-related attributes are updated so that that the rendered element is removed from the accessibility tree.
    */
-  decorative: ReadonlySignal<boolean | undefined>;
+  decorative: ReadonlySignal<boolean>;
 }
