9ce92245d12a402425b355049169400f1e544f3a
ZAHON <konrad.zahon@gmail.com>
2025-08-15 20:00:33 +0200

    docs(core): update `useLabelContext` hook JSDoc
    This change updates the JSDoc for the `useLabelContext` hook.
    The new description provides a clearer explanation of its purpose, clarifying that the hook provides access to the `Label` component's internal context and exposes a readonly signal.

--- core/src/components/label/hooks/use-label-context/use-label-context.ts
+++ core/src/components/label/hooks/use-label-context/use-label-context.ts
@@ -2,7 +2,8 @@ import { useContext } from '@builder.io/qwik';
 import { LabelContext } from '../../contexts';
 
 /**
- * A hook that gives access to a context object containing properties to interact with the label.
+ * A hook that provides access to the `Label` component's internal context,
+ * exposing a readonly signals to interact with and react to the component's state.
  */
 export const useLabelContext = () => {
   const { preventDblClickTextSelection } = useContext(LabelContext);
