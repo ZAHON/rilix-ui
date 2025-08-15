097ba128811ab47790d44141176ece310aba2d11
ZAHON <konrad.zahon@gmail.com>
2025-08-15 22:46:17 +0200

    docs(core): add JSDoc for `AspectRatioContext`
    Added a descriptive JSDoc to `AspectRatioContext` to explain its purpose

--- core/src/components/aspect-ratio/contexts/aspect-ratio-context/aspect-ratio-context.ts
+++ core/src/components/aspect-ratio/contexts/aspect-ratio-context/aspect-ratio-context.ts
@@ -1,4 +1,8 @@
 import type { AspectRatioContextValue } from './aspect-ratio-context.types';
 import { createContextId } from '@builder.io/qwik';
 
+/**
+ * Provides the context for the `AspectRatio` component, allowing descendant
+ * components to access internal state without prop drilling.
+ */
 export const AspectRatioContext = createContextId<AspectRatioContextValue>('rilix-ui-aspect-ratio-context');
