f8907ac1d799fb5571f2a1389a11231fa044d8a2
ZAHON <konrad.zahon@gmail.com>
2025-08-30 09:36:17 +0200

    docs(core): update `Dialog.Title` component JSDoc
    The JSDoc for the `Dialog.Title` component has been updated to provide clearer guidance on how to remove the title while maintaining accessibility.
    The updated documentation now includes an alternative method using `ids={{ title: undefined }}` on `Dialog.Root`, in addition to the existing method of passing `aria-labelledby={undefined}` to `Dialog.Content`.

--- core/src/components/dialog/parts/dialog-title/dialog-title.tsx
+++ core/src/components/dialog/parts/dialog-title/dialog-title.tsx
@@ -4,9 +4,10 @@ import { Render } from '@/_internal';
 import { useDialogContext } from '../../contexts';
 
 /**
- * An accessible title to be announced when the dialog is opened.
- * If you want to remove the title entirely, remove this part and pass `aria-labelledby={undefined}` to `Dialog.Content` component.
- * If you remove the title, ensure you provide a descriptive `aria-label` directly on the `Dialog.Content` component, to provide an accessible label.
+ * An accessible title to be announced when the dialog is opened. If you want to remove the title
+ * entirely, remove this part and pass `aria-labelledby={undefined}` to `Dialog.Content` component,
+ * or pass `ids={{ title: undefined }}` to `Dialog.Root`. If you remove the title, ensure you
+ * provide a descriptive `aria-label` directly on the `Dialog.Content` component to provide an accessible label.
  * Renders an `<h2>` element.
  */
 export const DialogTitle = component$<DialogTitleProps>((props) => {
